5c007756d7765fa09911678e8b5bb1f66053c854
unknown <laodou09@gmail.com>
2024-10-08 12:16:49 +0800

    code

--- application/common/controller/Backend.php
+++ application/common/controller/Backend.php
@@ -230,7 +230,6 @@ class Backend extends Controller
         $this->assign('admin', Session::get('admin'));
     }
     protected function assignModelOption($name = '',$value = ''){
-        //加载自定义配置  巩汉飞 2020/09/18
         $name = $name?:'model_options';
         $value = $value?:($this->model::$options??[]);
         $this->assign($name,$value);
